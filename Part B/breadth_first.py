98f11a81ba14b9ea8f66385f96e1de0474611dd3
James Townsend <j.a.townsend2000@gmail.com>
2020-02-18 18:49:55 +0000

    Potential breadth first search response

--- Part B/breadth_first.py
+++ Part B/breadth_first.py
@@ -9,6 +9,16 @@ import graph10
 def bfs(G, a, b):
     G.add_nodes_from(G.nodes(), label=-1)  # initialization of all labels
     G.nodes[a]['label'] = 0
+    i = 0
+    while G.nodes[b]['label'] == -1:
+        for u in G.nodes():
+            if G.nodes[u]['label'] == i:
+                for v in G[u]:
+                    if G.nodes[v]['label'] == -1:
+                        G.nodes[v]['label'] = i + 1
+        i += 1
+    return G.nodes[v]['label']
+
 
 
 G6 = graph6.Graph()
